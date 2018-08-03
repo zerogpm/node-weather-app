8ec3816ee610dd472f75825820a4d8bfb06f1d6a
zerogpm <zerogpm@gmail.com>
2018-08-03 09:11:39 -0700

    encoding user input

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 const request = require('request');
+const yargs = require('yargs');
 
+const argv = yargs.options({
+  address: {
+    demand: true,
+    alias: 'address',
+    describe: 'Address to fetch weather for',
+    string: true
+  }
+})
+.help()
+  .alias('help', 'h')
+  .argv;
+
+console.log(argv.address);
+let address = encodeURIComponent(argv.address);
 request({
-  url: 'https://maps.googleapis.com/maps/api/geocode/json?address=1301%20lombard%20steet%20philadelphia&key=AIzaSyB4_LCaoZYowjQGbzidNEWiQkzXBBmsufk',
+  url: `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=AIzaSyB4_LCaoZYowjQGbzidNEWiQkzXBBmsufk`,
   json: true
 }, (error, response, body) => {
   console.log(`Address: ${body.results[0].formatted_address}`);
